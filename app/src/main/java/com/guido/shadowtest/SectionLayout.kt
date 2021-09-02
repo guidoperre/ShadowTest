ca473fac11a2cd2254c67fc64fc3afef666249a1
Guido Perre <guido.perre@mercadolibre.com>
2021-09-02 17:50:40 -0300

    check

--- app/src/main/java/com/guido/shadowtest/SectionLayout.kt
+++ app/src/main/java/com/guido/shadowtest/SectionLayout.kt
@@ -5,9 +5,10 @@ import android.graphics.Canvas
 import android.graphics.Path
 import android.graphics.RectF
 import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
-
 
 class SectionLayout : ConstraintLayout {
 
@@ -19,9 +20,11 @@ class SectionLayout : ConstraintLayout {
     private val layoutRect = RectF()
     private val path = Path()
 
+    private val drawable = ContextCompat.getDrawable(context, R.drawable.wallet_api_sections_bg)
     private val cornerRadius = resources.getDimension(
         R.dimen.wallet_api_section_layout_corner_radius
     )
+
     private val horPad = resources.getDimension(
         R.dimen.wallet_api_section_layout_offset_left_right
     )
@@ -41,40 +44,74 @@ class SectionLayout : ConstraintLayout {
         R.dimen.wallet_api_section_layout_offset_bottom_gap
     )
 
-    constructor(context: Context): super(context) {
-        initBackground()
-    }
+    private val verticalPadding = resources.getDimension(
+        R.dimen.wallet_api_section_layout_padding_vertical
+    ).toInt()
+    private val horizontalPadding = resources.getDimension(
+        R.dimen.wallet_api_section_layout_padding_horizontal
+    ).toInt()
 
-    constructor(context: Context, attrs: AttributeSet): super(context, attrs) {
-        initBackground()
-    }
+    private var horizontalScaleRatio = 1f
+    private var verticalScaleRatio = 1f
+
+    constructor(context: Context): super(context)
+
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
 
     constructor(
         context: Context,
         attrs: AttributeSet,
         defStyleAttr: Int
-    ): super(context, attrs, defStyleAttr) {
-        initBackground()
-    }
+    ): super(context, attrs, defStyleAttr)
 
-    private fun initBackground() {
-        val drawable = ContextCompat.getDrawable(context, R.drawable.wallet_api_sections_bg)
+    init {
         background = drawable
     }
 
-    private fun setBackgroundScale(w: Int, h: Int) {
-        scaleX = ((horPad * TWO) + w) / w
-        scaleY = (botPad + topPad + h) / h
+    private fun setMargin() {
+        val params = layoutParams as MarginLayoutParams
+        params.setMargins(
+            horizontalPadding,
+            verticalPadding,
+            horizontalPadding,
+            verticalPadding
+        )
+        layoutParams = params
     }
 
-    private fun setViewGroupBounds(w: Int, h: Int) {
+    private fun setBackgroundScale() {
+        scaleX = horizontalScaleRatio
+        scaleY = verticalScaleRatio
+    }
+
+    private fun setChildScale(view: View) {
+//        view.scaleX = 1.5f
+//        view.scaleY = 1.5f
+//        if (view !is ViewGroup) {
+//            view.scaleX = 1 / horizontalScaleRatio
+//            view.scaleY = 1 / verticalScaleRatio
+//        } else {
+//            setChildViews(view)
+//        }
+    }
+
+    private fun setBounds(w: Int, h: Int) {
         path.reset()
-        layoutRect.top = topGap
-        layoutRect.left = horGap
-        layoutRect.right = w - horGap
-        layoutRect.bottom = h - botGap
+        layoutRect.right = w.toFloat() + 300
+        layoutRect.bottom = h.toFloat() + 200
         path.addRoundRect(layoutRect, cornerRadius, cornerRadius, Path.Direction.CW)
         path.close()
+    }
+
+    private fun setChildViews(view: ViewGroup) {
+        for (i in 0 until view.childCount) {
+            setChildScale(view.getChildAt(i))
+        }
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        setMargin()
     }
 
     override fun draw(canvas: Canvas?) {
@@ -84,14 +121,75 @@ class SectionLayout : ConstraintLayout {
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        setBackgroundScale(w, h)
-        setViewGroupBounds(w, h)
+        horizontalScaleRatio = ((horPad * TWO) + w) / w
+        verticalScaleRatio = (botPad + topPad + h) / h
+        setBounds(w, h)
+        setBackgroundScale()
+        setChildViews(this)
     }
 
     override fun dispatchDraw(canvas: Canvas) {
         val save = canvas.save()
-        canvas.clipPath(path)
+        //canvas.clipPath(path)
         super.dispatchDraw(canvas)
         canvas.restoreToCount(save)
+    }
+
+    override fun addView(child: View?) {
+        super.addView(child)
+        child?.let {
+            setChildScale(it)
+        }
+    }
+
+    override fun addView(child: View?, index: Int) {
+        super.addView(child, index)
+        child?.let {
+            setChildScale(it)
+        }
+    }
+
+    override fun addView(child: View?, params: ViewGroup.LayoutParams?) {
+        super.addView(child, params)
+        child?.let {
+            setChildScale(it)
+        }
+    }
+
+    override fun addView(child: View?, index: Int, params: ViewGroup.LayoutParams?) {
+        super.addView(child, index, params)
+        child?.let {
+            setChildScale(it)
+        }
+    }
+
+    override fun addView(child: View?, width: Int, height: Int) {
+        super.addView(child, width, height)
+        child?.let {
+            setChildScale(it)
+        }
+    }
+
+    override fun addViewInLayout(
+        child: View?,
+        index: Int,
+        params: ViewGroup.LayoutParams?
+    ): Boolean {
+        child?.let {
+            setChildScale(it)
+        }
+        return super.addViewInLayout(child, index, params)
+    }
+
+    override fun addViewInLayout(
+        child: View?,
+        index: Int,
+        params: ViewGroup.LayoutParams?,
+        preventRequestLayout: Boolean
+    ): Boolean {
+        child?.let {
+            setChildScale(it)
+        }
+        return super.addViewInLayout(child, index, params, preventRequestLayout)
     }
 }
